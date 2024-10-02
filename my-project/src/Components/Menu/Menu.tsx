b95fc17c138dde34f6a06d2a681f7a006bf40fc2
B3rg4r4 <pedrobergara5@gmail.com>
2024-10-01 23:34:47 -0300

    criacao completa do menu

--- my-project/src/Components/Menu/Menu.tsx
+++ my-project/src/Components/Menu/Menu.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
+
 export default function Menu() {
-    return (
-        <header className="menu">
-            <h1>
-                Menu
-            </h1>
-        </header>
-    )
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="../../app/pagina/pagina1/page.tsx">Primeira Ideia</Link>
+        </li>
+        <li>
+            <Link href="../../app/pagina/pagina2/page.tsx">Segunda Ideia</Link>
+        </li>
+        <li>
+            <Link href="../../app/pagina/pagina3/page.tsx">Terceira Ideia</Link>
+        </li>
+        <li>
+            <Link href="../../app/pagina/pagina4/page.tsx">Calculo</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
