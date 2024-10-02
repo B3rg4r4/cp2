a26c8ce441d8760341e6f0fde20576794bda3dc3
Andenik <andenik@yahoo.com.br>
2024-10-01 22:04:49 -0300

    criando componentes padroes

---
+++ my-project/src/Components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,17 @@
+export default function Cabecalho() {
+  return (
+    <header>
+      <h1>Meu site</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/sobre">Sobre</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ my-project/src/Components/Menu/Menu.tsx
@@ -0,0 +1,16 @@
+export default function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          {" "}
+          <Link href="/">Home</Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link href="/Paginas">Produtos</Link>{" "}
+        </li>
+      </ul>
+    </nav>
+  );
+}
