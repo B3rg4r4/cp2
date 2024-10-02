613b9b0a17185645b3ac8bbefc1b4a9fe18ac15a
Andenik <andenik@yahoo.com.br>
2024-10-01 23:55:23 -0300

    colocado api em todas as paginas

--- my-project/src/app/pagina/pagina3/page.tsx
+++ my-project/src/app/pagina/pagina3/page.tsx
@@ -1,12 +1,14 @@
-
+import NasaImage from "@/Components/NasaImg/NasaImg";
 export default function PaginaTres() {
-    return (
-      <div>
-        <h1>Reinterpretação de Mitologia e Textos Antigos</h1>
-        <p>
-         Velikovsky faz conexões entre suas teorias de catástrofes cósmicas e relatos antigos de diferentes culturas. Ele acredita que mitologias, lendas e textos religiosos, como a Bíblia, descrevem eventos reais causados por essas interações astronômicas. Para ele, histórias como o Êxodo, o Dilúvio e as batalhas dos deuses são reminiscências de desastres cósmicos. 
-        </p>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>Reinterpretação de Mitologia e Textos Antigos</h1>
+      <p>
+        Velikovsky faz conexões entre suas teorias de catástrofes cósmicas e relatos antigos de diferentes culturas. Ele acredita que mitologias,
+        lendas e textos religiosos, como a Bíblia, descrevem eventos reais causados por essas interações astronômicas. Para ele, histórias como o
+        Êxodo, o Dilúvio e as batalhas dos deuses são reminiscências de desastres cósmicos.
+      </p>
+      <NasaImage />
+    </div>
+  );
+}
--- my-project/src/app/pagina/pagina4/page.tsx
+++ my-project/src/app/pagina/pagina4/page.tsx
@@ -1,12 +1,10 @@
-
+import NasaImage from "@/Components/NasaImg/NasaImg";
 export default function PaginaQuatro() {
-    return (
-      <div>
-        <h1>Calculo</h1>
-        <p>
-          Ele nao desenvolveu um codigo mas criou teorias e ideias em cima de pensamentos sobre 
-        </p>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>Calculo</h1>
+      <p>Ele nao desenvolveu um codigo mas criou teorias e ideias em cima de pensamentos sobre</p>
+      <NasaImage />
+    </div>
+  );
+}
