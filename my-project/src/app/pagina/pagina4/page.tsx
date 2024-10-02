ff18c8f3f62b50ce0d5e1d6b6d1620ccca0cdb82
B3rg4r4 <pedrobergara5@gmail.com>
2024-10-01 23:31:32 -0300

    criacao da pagina4

---
+++ my-project/src/app/pagina/pagina4/page.tsx
@@ -0,0 +1,12 @@
+
+export default function PaginaQuatro() {
+    return (
+      <div>
+        <h1>Calculo</h1>
+        <p>
+          Ele nao desenvolveu um codigo mas criou teorias e ideias em cima de pensamentos sobre 
+        </p>
+      </div>
+    );
+  }
+  
